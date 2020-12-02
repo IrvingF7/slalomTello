40eb32a9cd9ededd7fd984e762fe9be70db40337
Braeden <braeden@berkeley.edu>
2020-12-01 21:13:00 -0800

    added the matlab plotting scripts

---
+++ plots.m
@@ -0,0 +1,69 @@
+clear all
+close all
+d = testDataDecoder("statedata_PID.txt")
+
+%%
+close all
+figure
+plot(d.time,d.velocity_x,d.time,d.velocity_y,d.time,d.velocity_z)
+xlabel('Time (s)')
+ylabel('Velocity (cm/s)')
+legend('x','y','z')
+title('Velocities')
+
+figure
+plot(d.time,d.pitch,d.time,d.roll,d.time,d.yaw)
+xlabel('Time (s)')
+ylabel('Angle (degrees)')
+legend('Pitch','Roll','Yaw')
+ylim([-180 180])
+title('Angles')
+
+figure
+plot(d.time,d.tvec_x)
+hold on
+plot(d.time,d.tvec_y)
+plot(d.time,d.tvec_z)
+hold off
+legend('tvec x','tvec y','tvec z')
+title('Translation vectors')
+
+figure
+subplot(2,1,1)
+plot(d.time,d.tvec_x,d.time,d.ref_x)
+title('X translation vector')
+legend('X position','X reference')
+subplot(2,1,2)
+plot(d.time,-d.control_leftRight)
+title('Control LR')
+
+figure
+subplot(2,1,1)
+plot(d.time,d.tvec_y,d.time,d.ref_y)
+title('Y translation vector')
+legend('Y position','Y reference')
+subplot(2,1,2)
+plot(d.time,d.control_upDown)
+title('Control UD')
+
+figure
+subplot(2,1,1)
+plot(d.time,d.tvec_z,d.time,d.ref_z)
+title('Z translation vector')
+legend('Z position','Z reference')
+subplot(2,1,2)
+plot(d.time,-d.control_forwardBack)
+title('Control FB')
+
+figure
+plot(d.time,d.rvec_y)
+hold on
+plot(d.time,d.rvec_z)
+hold off
+legend('rvec y','rvec z')
+title('Rotation vectors')
+
+figure
+plot(d.time,d.rvec_x)
+legend('rvec x')
+title('Rotation vectors')
---
+++ testDataDecoder.m
@@ -0,0 +1,60 @@
+function data = testDataDecoder(file)
+
+% index                        1
+% time                         2
+% reference input              3
+% control left/right           4
+% control forward/backward     5
+% control up/down              6
+% control yaw                  7
+% pitch                        8
+% roll                         9
+% yaw                         10
+% vgx                         11
+% vgy                         12
+% vgz                         13
+% templ                       14
+% temph                       15
+% tof                         16
+% h                           17
+% bat                         18
+% baro                        19
+% time                        20
+% agx                         21
+% agy                         22
+% agz                         23
+
+values = csvread(file,3,0);
+
+data.time                = values(:,2);
+data.ref_yaw           = values(:,3);
+data.control_leftRight   = values(:,4);
+data.control_forwardBack = values(:,5);
+data.control_upDown      = values(:,6);
+data.control_yaw         = values(:,7);
+data.pitch               = values(:,8);
+data.roll                = values(:,9);
+data.yaw                 = values(:,10);
+data.velocity_x          = values(:,11);
+data.velocity_y          = values(:,12);
+data.velocity_z          = values(:,13);
+data.temperature_low     = values(:,14);
+data.temperature_high    = values(:,15);
+data.timeOfFlight        = values(:,16);
+data.height              = values(:,17);
+data.battery             = values(:,18);
+data.barometer           = values(:,19);
+data.acceleration_x      = values(:,21);
+data.acceleration_y      = values(:,22);
+data.acceleration_z      = values(:,23);
+data.tvec_x      = values(:,24);
+data.tvec_y      = values(:,25);
+data.tvec_z      = values(:,26);
+data.rvec_x      = values(:,27);
+data.rvec_y      = values(:,28);
+data.rvec_z      = values(:,29);
+data.ref_x = values(:,30);
+data.ref_y = values(:,31);
+data.ref_z = values(:,32);
+
+end
